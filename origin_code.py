670810aedf82ffcdc9c97e7f62f142b787d42e94
WeiQiuhong <matthewwqh25@gmail.com>
2026-01-04 01:26:43 +0800

    20260104更新：完成对txt数据的处理，将其转换为dataframe

--- origin_code.py
+++ origin_code.py
@@ -7,10 +7,36 @@ import os
 file_path = "Table.txt"
 _, ext = os.path.splitext(file_path)
 if ext == ".txt":
-    # 获取第一行作为表头,剩下的作为数据
     with open(file_path, "r") as f:
+        # 处理表头
         header = f.readline()
-        print(header)
-        data =  f.readlines()
-        print(data)
+        header_list = header.split()
+        # 处理数据
+        data_origin =  f.readlines()
+        # 移除每行末尾的换行符
+        data_move_t = [line.rstrip() for line in data_origin]
+        # 移除空行
+        data_move_empty_line = [line for line in data_move_t if line.strip()]
+        # 将每行内的数据根据空格分割
+        data_lines = [line.split() for line in data_move_empty_line]
+        df = pd.DataFrame(data_lines, columns=header_list)
+        print(df)
+        # print(data_lines)
+
+        # for line in data_move_empty_line:
+        #     print(line.split())
+        #     print(len(line.split()))
+
+        # data_list = []
+        # for line in data:
+        #     print(line.strip())
+        #     data_list.append(line.strip())
+        # print(data_list)
+        # for line in data:
+        #     if line
+        #     print(line.splitlines())
+        # print(data)
+        # print(type(data))
+
+
 
--- target_design.py
+++ target_design.py
@@ -6,12 +6,22 @@ import pandas as pd
 class StockAnalysis:
     def get_data(self, file_path: str) -> pd.DataFrame:
         """
-        :param file_path: 文件路径
-        :return: pandas DataFrame
+        :param file_path: 导入的数据，一般为某个特定股票的近两年数据
+        :return: dataframe: 处理好的数据框架，表头分别为：时间、开盘、最高、最低、收盘	、涨幅、振幅、总手、金额、换手%、成交次数
         """
         _, ext = os.path.splitext(file_path)
         if ext == ".txt":
-            # 获取第一行作为表头
             with open(file_path, "r") as f:
+                # 处理表头
                 header = f.readline()
+                header_list = header.split()
+                # 处理数据
+                data_origin = f.readlines()
+                # 移除每行末尾的换行符
+                data_move_t = [line.rstrip() for line in data_origin]
+                # 移除空行
+                data_move_empty_line = [line for line in data_move_t if line.strip()]
+                # 将每行内的数据根据空格分割
+                data_lines = [line.split() for line in data_move_empty_line]
+                df = pd.DataFrame(data_lines, columns=header_list)
         return df
